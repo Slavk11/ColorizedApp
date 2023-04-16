d4323e67a22528de0ecbd68d38a091379c5f3de6
Stanislav Sazonov <slavk11@yandex.ru>
2023-04-16 09:57:37 +0300

    added logic

--- ColorizedApp/MainViewController.swift
+++ ColorizedApp/MainViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+protocol SettingsViewControllerDelegate: AnyObject {
+    func setColor(_ color: UIColor)
 }
 
+final class MainViewController: UIViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        settingsVC.delegate = self
+        settingsVC.viewColor = view.backgroundColor
+    }
+}
+
+// MARK: - SettingsViewControllerDelegate
+extension MainViewController: SettingsViewControllerDelegate {
+    func setColor(_ color: UIColor) {
+        view.backgroundColor = color
+    }
+}
